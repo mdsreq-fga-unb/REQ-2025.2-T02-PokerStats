59142cec34a3e268d1cb51538f325c8163adcb25
felipeJRdev <felipe123rodrigues1@gmail.com>
2025-11-28 21:07:40 -0300

    feat: calculo do ROI geral e por tipo de torneio.

--- src/pokerstats/core/service.py
+++ src/pokerstats/core/service.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 from .models import TransacaoDTO, HandHistoryDTO, TorneioConsolidado
 from .readers.transactions import ler_transacoes_excel
 from .readers.hand_history import processar_lote_hhs
@@ -23,10 +23,8 @@ class BodogService:
 
     def consolidar_dados(self, transacoes: List[TransacaoDTO], hhs: List[HandHistoryDTO]) -> Tuple[List[TorneioConsolidado], int]:
         consolidados = []
-        
-        JANELA_HORAS = 0.33
+        JANELA_HORAS = 0.75
         TOLERANCIA_VALOR = 0.10
-
         candidatos = []
 
         for t in transacoes:
@@ -35,35 +33,24 @@ class BodogService:
                 diff_valor = abs(t.buy_in - hh.buy_in_estimado)
                 
                 if diff_horas <= JANELA_HORAS and diff_valor <= TOLERANCIA_VALOR:
-                    candidatos.append({
-                        'diff_tempo': diff_horas,
-                        'transacao': t,
-                        'hh': hh
-                    })
+                    candidatos.append({'diff_tempo': diff_horas, 'transacao': t, 'hh': hh})
 
         candidatos.sort(key=lambda x: x['diff_tempo'])
-
         transacoes_usadas = set()
         hhs_usados = set()
 
         for c in candidatos:
-            t = c['transacao']
-            hh = c['hh']
-            
-            id_t = id(t)
-            id_h = id(hh)
-
-            if id_t not in transacoes_usadas and id_h not in hhs_usados:
+            t, hh = c['transacao'], c['hh']
+            if id(t) not in transacoes_usadas and id(hh) not in hhs_usados:
                 consolidados.append(TorneioConsolidado("VINCULADO", t, hh))
-                transacoes_usadas.add(id_t)
-                hhs_usados.add(id_h)
+                transacoes_usadas.add(id(t))
+                hhs_usados.add(id(hh))
 
         for t in transacoes:
             if id(t) not in transacoes_usadas:
                 consolidados.append(TorneioConsolidado("PENDENTE_HH", t))
 
         qtd_descartados = len(hhs) - len(hhs_usados)
-            
         return consolidados, qtd_descartados
 
     def salvar_no_banco(self, lista_consolidados: List[TorneioConsolidado]):
@@ -71,17 +58,70 @@ class BodogService:
     
     def obter_historico_banco(self):
         return self.repo.listar_todos()
-    
+
     def obter_resumo_financeiro(self):
         somas = self.repo.obter_somas_totais()
-        
         total_buyin = somas[0] if somas[0] else 0.0
         total_premio = somas[1] if somas[1] else 0.0
-        
-        total_lucro = total_premio - total_buyin
-        
-        return {
-            "total_buyin": total_buyin,
-            "total_premio": total_premio,
-            "total_lucro": total_lucro
+        return {"total_buyin": total_buyin, "total_premio": total_premio, "total_lucro": total_premio - total_buyin}
+
+    def gerar_relatorio_roi(self) -> Dict:
+        """
+        Processa todos os dados do banco para gerar ROI Geral e Segmentado.
+        """
+        todos_torneios = self.repo.listar_todos()
+
+        stats = {
+            "Geral": {"investido": 0.0, "retorno": 0.0},
+            "MTT": {"investido": 0.0, "retorno": 0.0},
+            "Sit & Go": {"investido": 0.0, "retorno": 0.0},
+            "Outros": {"investido": 0.0, "retorno": 0.0}
         }
+
+        for t in todos_torneios:
+            inv = t.buy_in or 0.0
+            ret = t.premio or 0.0
+            
+            stats["Geral"]["investido"] += inv
+            stats["Geral"]["retorno"] += ret
+            
+            nome = (t.nome_torneio or "").upper()
+            
+            if inv == 0 and ret > 0:
+                categoria = "Outros"
+            elif "SIT" in nome and "GO" in nome:
+                categoria = "Sit & Go"
+            elif "SNG" in nome:
+                categoria = "Sit & Go"
+            elif "MTT" in nome or "GUARANTEED" in nome or "GTD" in nome:
+                categoria = "MTT"
+            elif "CASH" in nome:
+                categoria = "Outros"
+            else:
+                categoria = "MTT" 
+
+            if categoria not in stats:
+                stats[categoria] = {"investido": 0.0, "retorno": 0.0}
+                
+            stats[categoria]["investido"] += inv
+            stats[categoria]["retorno"] += ret
+
+        relatorio_final = {}
+        for cat, dados in stats.items():
+            investido = dados["investido"]
+            retorno = dados["retorno"]
+            lucro = retorno - investido
+            
+            if investido > 0:
+                roi_pct = ((retorno - investido) / investido) * 100
+            else:
+                roi_pct = 0.0 
+            
+            relatorio_final[cat] = {
+                "investido": investido,
+                "retorno": retorno,
+                "lucro": lucro,
+                "roi": roi_pct
+            }
+            
+        return relatorio_final
